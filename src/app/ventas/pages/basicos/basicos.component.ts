81ee86c5478ab3bf147ac6a5410cbb0d9586f063
WiiruxCruz <hialf.92@gmail.com>
2021-12-10 17:25:55 -0600

    Manejo de Date pipe

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -10,4 +10,6 @@ export class BasicosComponent {
   nombreLower: string = 'alfredo';
   nombreUpper: string = 'ALFREDO';
   nombreCompleto: string = 'aLFredO rAmIreZ';
+
+  fecha: Date = new Date();
 }
